0e5ad80199009763d6c43b25f1b6270bdbc64e80
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-03-09 19:49:03 +0200

    feat: update layout

--- lab1/app/_layout.tsx
+++ lab1/app/_layout.tsx
@@ -4,17 +4,34 @@ import {StatusBar} from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import {useColorScheme} from '@/hooks/useColorScheme';
+import {StyleSheet, Text, View} from "react-native";
 
 export default function RootLayout() {
     const colorScheme = useColorScheme();
 
     return (
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <View style={styles.headingContainer}>
+                <Text>
+                    FirstMobileApp
+                </Text>
+            </View>
             <Stack>
                 <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
                 <Stack.Screen name="+not-found"/>
             </Stack>
             <StatusBar style="auto"/>
+            <View>
+                <Text>
+                    Шумський Олександр Вячеславович ВТ-21-1
+                </Text>
+            </View>
         </ThemeProvider>
     );
 }
+
+const styles = StyleSheet.create({
+    headingContainer: {
+        paddingTop: 30,
+    }
+})
