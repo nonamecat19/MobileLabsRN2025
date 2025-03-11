39ca7b7348308ae681926fc2c8455f87302cba35
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-03-11 10:11:21 +0200

    feat: init lab2

---
+++ lab2/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import {StyleSheet, View} from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
