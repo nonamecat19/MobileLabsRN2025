83b6f83d554c2d28423087a50eefc219be9a6517
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-03-11 11:20:31 +0200

    feat: added theme provider

--- lab2/app/_layout.tsx
+++ lab2/app/_layout.tsx
@@ -4,6 +4,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import {StatusBar} from 'expo-status-bar';
 import {useEffect} from 'react';
 import 'react-native-reanimated';
+import {ThemeProvider} from "@/providers";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 void SplashScreen.preventAutoHideAsync();
@@ -24,11 +25,11 @@ export default function RootLayout() {
     }
 
     return (
-        <>
+        <ThemeProvider>
             <Stack>
                 <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
             </Stack>
             <StatusBar style="auto"/>
-        </>
+        </ThemeProvider>
     );
 }
---
+++ lab2/providers/ThemeProvider.tsx
@@ -0,0 +1,55 @@
+import {ThemeProvider as StyledThemeProvider} from 'styled-components/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {createContext, ReactNode, useEffect, useState} from "react";
+import {AppTheme} from "@/types";
+import {appThemes, DarkTheme} from "@/themes";
+import {THEME_STORAGE_KEY, ThemeName} from "@/constants";
+
+type ThemeContextValue = {
+    theme: AppTheme
+    themeName: ThemeName
+    toggle: () => void
+}
+
+export const ThemeContext = createContext<ThemeContextValue>({
+    theme: DarkTheme,
+    themeName: ThemeName.Dark,
+    toggle: () => {
+    }
+})
+
+type ThemeProviderProps = {
+    children: ReactNode;
+}
+
+export function ThemeProvider({children}: ThemeProviderProps) {
+    const [themeName, setThemeName] = useState<ThemeName>(ThemeName.Dark);
+
+    useEffect(() => {
+        async function loadTheme() {
+            const storedTheme = await AsyncStorage.getItem(THEME_STORAGE_KEY) as ThemeName | null;
+            if (!storedTheme || !appThemes[storedTheme]) return
+            setThemeName(storedTheme);
+        }
+        void loadTheme();
+    }, []);
+
+    async function toggle() {
+        const newTheme = themeName === ThemeName.Light ? ThemeName.Dark : ThemeName.Light;
+        setThemeName(newTheme);
+        await AsyncStorage.setItem(THEME_STORAGE_KEY, newTheme);
+    }
+
+    const theme = appThemes[themeName];
+
+    return (
+        <ThemeContext.Provider
+            value={{themeName, theme, toggle}}
+        >
+            <StyledThemeProvider theme={theme}>
+                {children}
+            </StyledThemeProvider>
+        </ThemeContext.Provider>
+    );
+}
+
