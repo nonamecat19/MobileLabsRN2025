dc746d425834cfb03297a36822096d3df888c293
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-03-21 12:11:58 +0200

    fix: community screen paddings

--- lab2/components/News/News.tsx
+++ lab2/components/News/News.tsx
@@ -1,6 +1,7 @@
-import {FlatList, View} from "react-native";
+import {FlatList} from "react-native";
 import {CommunityNewsItem} from "@/types";
-import { NewsItem } from "./NewsItem";
+import {NewsItem} from "./NewsItem";
+import styled from "styled-components/native";
 
 interface NewsProps {
     data: CommunityNewsItem[]
@@ -8,17 +9,22 @@ interface NewsProps {
 
 export function News({data}: NewsProps) {
     return (
-        <FlatList
-            data={data}
-            keyExtractor={(_, index) => index.toString()}
-            numColumns={1}
-            renderItem={({item, index}) => (
-                <NewsItem
-                    key={index}
-                    {...item}
-                />
-            )}
-            // contentContainerStyle={styles.container}
-        />
+        <ListContainer>
+            <FlatList
+                data={data}
+                keyExtractor={(_, index) => index.toString()}
+                numColumns={1}
+                renderItem={({item, index}) => (
+                    <NewsItem
+                        key={index}
+                        {...item}
+                    />
+                )}
+            />
+        </ListContainer>
     )
 }
+
+const ListContainer = styled.View`
+    padding-vertical: 20px;
+`
