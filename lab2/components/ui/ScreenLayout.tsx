5143e55d70cb20ee115420ad5a15cbcb04b3a8d1
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-03-21 11:46:40 +0200

    fix: nested FlatList issue

--- lab2/components/ui/ScreenLayout.tsx
+++ lab2/components/ui/ScreenLayout.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components/native";
 import {StyledThemeProps} from "@/types";
+import { ScrollView } from 'react-native-virtualized-view'
 
-export const ScreenLayout = styled.ScrollView`
+export const ScreenLayout = styled(ScrollView)`
     background-color: ${({theme}: StyledThemeProps) => theme.background};
     padding-top: 35px;
 `
